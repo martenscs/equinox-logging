20c5d3bf38d3f5ac089440c7d82396d8d61c7b31
cmartens <cmartens@DAL-CMARTENS1.tmwsystems.com>
2014-10-27 16:52:36 -0500

    Initial Project

---
+++ net.martenscs.osgi.logging.sample/src/net/martenscs/osgi/logging/sample/Activator.java
@@ -0,0 +1,44 @@
+package net.martenscs.osgi.logging.sample;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Activator implements BundleActivator {
+
+	protected final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		LOGGER.trace("Test Trace");
+		LOGGER.debug("Test Debug");
+		LOGGER.info("Test Info");
+		LOGGER.warn("Test Warn");
+		LOGGER.error("Test Error");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
